9343a39606df593b9baa55632cc6152e3c12e785
Meirasulin <asulinmeir64@gmail.com>
2023-11-20 10:28:08 +0200

    meir test commit

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 
+router.post("/register", handleUserRegistration);
+router.post("/login", handleLogin);
+
 router.use("*", (req: Request, res: Response) =>
   res.status(404).send("Page not found!")
 );
