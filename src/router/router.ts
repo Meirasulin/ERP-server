aa0c1b4212a56a436018016658712b54adbb0fd3
itzik <itsikzigi@gmail.com>
2023-11-20 12:09:21 +0200

    initialed basic connection for get all products req

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
+import internalRouter from "../internalRequests/router/internalReqRouter";
 const router = express.Router();
+
+router.use("/api/inventory", internalRouter);
 
 router.use("*", (req: Request, res: Response) =>
   res.status(404).send("Page not found!")
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 import "dotenv/config";
 import connectionToMongoDb from "./dbAccess/mongoDBConnection";
 import { connectionToPostgres } from "./dbAccess/postgresConnection";
+import cors from "./cors/cors";
+import morgan from "./logger/morgan";
+import router from "./router/router";
+
 const app = express();
+
+app.use(morgan);
+app.use(cors);
+app.use(express.json());
+app.use(router);
 
 const PORT = process.env.PORT;
 
